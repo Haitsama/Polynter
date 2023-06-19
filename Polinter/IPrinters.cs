ac774c78bc733b6c4a5f2599c48ca7fd12e75fd7
Haitsama <haitsam@students.amikom.ac.id>
2023-06-19 19:10:37 +0700

    Add project files.

---
+++ Polinter/IPrinters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinter
+{
+    interface IPrinters
+    {
+        void Print(Printer printer);
+        void Show(Printer printer);
+    }
+}
---
+++ Polinter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            Console.WriteLine("Silahkan pilih jenis Printer berikut ini : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.Write("Pilih nomor 1..3 :   ");
+            int pilihan = Convert.ToInt32(Console.ReadLine());
+            printer.Merk = pilihan;
+
+            IPrinters show;
+            IPrinters print;
+
+            if (printer.Merk == 1)
+            {
+                show = new Epson();
+                print = new Epson();
+            }
+            else if (printer.Merk == 2)
+            {
+                show = new Canon();
+                print = new Canon();
+            }              
+            else 
+                show = new Laserjet();
+                print = new Laserjet();
+
+            show.Show(printer);
+            print.Print(printer);
+            Console.ReadLine();
+        }
+    }
+}
