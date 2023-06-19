cd3b55f2188e13e9b35b3b7ad0ccf2fa3e3bbcde
Haitsama <haitsam@students.amikom.ac.id>
2023-06-19 19:10:37 +0700

    Pertemuan10

---
+++ Polinter/Epson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinter
+{
+    public class Epson : IPrinters
+    {
+        void IPrinters.Print(Printer printer)
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+
+        void IPrinters.Show(Printer printer)
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+    }
+}
---
+++ Polinter/Laserjet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polinter
+{
+    public class Laserjet : IPrinters
+    {
+        void IPrinters.Print(Printer printer)
+        {
+            Console.WriteLine("LasetJet printer printing....");
+        }
+
+        void IPrinters.Show(Printer printer)
+        {
+            Console.WriteLine("LasetJet display dimension : 11*12");
+        }
+    }
+}
